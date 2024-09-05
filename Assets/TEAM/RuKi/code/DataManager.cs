f6fa06305970af982634efba2ca50d819beaeb41
RuKe23 <143714641+RuKe23@users.noreply.github.com>
2024-09-05 16:23:50 +0900

    데이터 매니저(가변 데이터 관리)

--- Assets/TEAM/RuKi/code/DataManager.cs
+++ Assets/TEAM/RuKi/code/DataManager.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
+[System.Serializable]
+public class UnitList
+{
+    public int level;
+    public bool unlock;
+}
+
+[System.Serializable]
+public class ItemList
+{
+    public float level;
+    public float progress;
+}
+
 public class DataManager : MonoBehaviour
 {
     public static DataManager Instance;
@@ -20,5 +34,19 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    public int score;
+    [Header("List Data")]
+    [SerializeField]
+    public UnitList[] unitList;
+    [SerializeField]
+    public ItemList[] itemList;
+
+    [Header("Money Data")]
+    public int organic;
+    public int coin;
+    
+    [Header("etc")]
+    public UnitData[] combatUnit;
+    public bool[] stageClear;
+    public float[] challengeProgress;
+    public int storyProgress;
 }
