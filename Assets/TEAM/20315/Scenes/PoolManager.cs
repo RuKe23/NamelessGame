1a1f87a0896ec15ba445c1b69d0a58da815a20e3
uygmin <23-11004@jangyu-h.gne.go.kr>
2024-09-05 18:05:05 +0900

    오브젝트 폴 생성(주석삭제)

--- Assets/TEAM/20315/Scenes/PoolManager.cs
+++ Assets/TEAM/20315/Scenes/PoolManager.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    // 프리펩 보관 변수
     public GameObject[] prefabs;
-    // 풀 담당 리스트
     List<GameObject>[] pools;   
 
     void Awake()
     {
-        // 풀 안에 있는 배열 초기화
         pools = new List<GameObject>[prefabs.Length];
 
         for (int index = 0; index < pools.Length; index++) {
-            // 배열 안에 있는 리스트 초기화
             pools[index] = new List<GameObject>();
         }
     }
-
+    
     public GameObject Get(int index)
     {
         GameObject select = null;
