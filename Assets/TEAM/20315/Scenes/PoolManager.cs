566de1e5bfa6096a537067a4fc2a3b9d47255b94
uygmin <23-11004@jangyu-h.gne.go.kr>
2024-09-09 16:06:15 +0900

    풀링 함수 추가

--- Assets/TEAM/20315/Scenes/PoolManager.cs
+++ Assets/TEAM/20315/Scenes/PoolManager.cs
@@ -20,6 +20,23 @@ public class PoolManager : MonoBehaviour
     {
         GameObject select = null;
 
+        foreach (GameObject item in pools[index])
+        {
+
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (!select) 
+        {
+            select = Instantiate(prefabs[index], transform);
+            pools[index].Add(select);
+        }
+
         return select;
     }
 }
