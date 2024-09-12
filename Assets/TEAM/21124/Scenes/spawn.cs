e557340b87eb5908913b978e536f44c5d906cc87
10be <136049120+10be@users.noreply.github.com>
2024-09-12 20:17:54 +0900

    프리팹 자동생성

--- Assets/TEAM/21124/Scenes/spawn.cs
+++ Assets/TEAM/21124/Scenes/spawn.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
+    public Transform[] spawnPoint;
+
+    float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) {
-            GameManager.instance.pool.Get(0);
-        }    
+        // .. 버튼 감지에 따른 프리펩 생성
+        // .. 아군 생성 로직은 구상됨, but 적 생성은 아직 모름
+        // .. 아군, 적군에 따라 생성 위치 다르게 조절
+        timer += Time.deltaTime;
+
+        if (timer > 0.2f) {
+            timer = 0;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        GameManager.instance.pool.Get(0);
     }
 }
