233dba0f0e151c5c5d2d5adc81c99a4e148bc099
uygmin <23-11004@jangyu-h.gne.go.kr>
2024-09-23 17:34:50 +0900

    주석 제거

--- Assets/TEAM/21124/Scenes/UnitSpawn.cs
+++ Assets/TEAM/21124/Scenes/UnitSpawn.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class UnitSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Transform spawnPoint;
 
     public int spawnunit;
